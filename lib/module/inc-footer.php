5561989ba15f8dd348ef1352fe16d202eb851e9e
OniChan <75073433+Savatachi@users.noreply.github.com>
2021-12-11 10:52:40 +0500

    Add files via upload

---
+++ lib/module/inc-footer.php
@@ -0,0 +1,46 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Социальные сети</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="#" target="_blank">
+										<span>Discord</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="#" target="_blank">
+										<span>YouTube</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
---
+++ lib/module/inc-menu.php
@@ -0,0 +1,163 @@
+<div class='menu-con-release'>
+	<span>AmoX Company 2021.05.09</span>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Ночная тема - On/Off</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Прозрачность - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Частицы - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Пульсация - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Волны - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<a href='/about'>
+			<div class="menu-btn-select">
+				<span>Информация</span>
+			</div>
+			<a href='/401'>
+			<div class="menu-btn-select">
+				<span>FAQs</span>
+			</div>
+			</a>
+			<a href='https://discord.gg/RxCtgWAYdX' target="_blank">
+			<div class="menu-btn-select">
+				<span>Discord</span>
+			</div>
+			</a>
+			<a href='/401' target="_blank">
+			<div class="menu-btn-select">
+				<span>YouTube</span>
+			</div>
+			</a>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Основное</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Главная страница</span>
+				</div>
+				</a>
+				<a href="/about">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<a href="/401">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>FAQs</span>
+				</div>
+				</a>
+				<div class='mobile-menu-tx1-group'>
+					<span>Соц. Сети</span>
+				</div>
+				<a href="#" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<a href="/401">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>YouTube</span>
+				</div>
+				</a>
+				<div class='mobile-menu-btn-spacer'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
